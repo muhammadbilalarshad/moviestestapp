a66060c8d113e088785f21df0e285bf6157512e9
BilalAwanCodenDevs <139571637+BilalAwanCodenDevs@users.noreply.github.com>
2023-08-24 03:32:42 +0500

    First Commit

---
+++ MoviesTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesTestApp
+//
+//  Created by ZONE-1 on 24/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
